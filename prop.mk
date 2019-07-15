5b41c37b1cd219f8ea28f4417c1e1d66ce40cab7
Yifan Hong <elsk@google.com>
2019-07-15 16:43:26 -0700

    vince: Replace BOARD_CHARGER_ENABLE_SUSPEND with ro.charger.enable_suspend
    Test: charger mode
    Bug: 124118169
    Change-Id: I3825d08e38d677bd7154879609857de302348b6f
    Signed-off-by: rayrzy <vrayananda@yahoo.com>

--- prop.mk
+++ prop.mk
@@ -58,6 +58,10 @@ persist.vendor.qti.telephony.vt_cam_interface=1 \
 vendor.camera.lowpower.record.enable=1 \
 vidc.enc.dcvs.extra-buff-count=2
 
+# Charger
+PRODUCT_PRODUCT_PROPERTIES += \
+ro.charger.enable_suspend=true
+
 # Cne
 PRODUCT_PROPERTY_OVERRIDES += \
 persist.vendor.cne.feature=1
