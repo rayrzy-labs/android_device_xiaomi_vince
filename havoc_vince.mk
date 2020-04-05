d5ed1705f64d976dbfab2dd464a01e5f68d6aa54
Marc Bourgoin <themard85@gmail.com>
2020-04-04 23:17:39 -0700

    vince: remove TARGET_VENDOR flag
    Change-Id: Ia9cf49e9d5aa775255c0a4fe5d681eebe48ed5ee
    Signed-off-by: rayrzy <vrayananda@yahoo.com>

--- havoc_vince.mk
+++ havoc_vince.mk
@@ -20,7 +20,6 @@ PRODUCT_NAME := havoc_vince
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 5 Plus
 PRODUCT_MANUFACTURER := Xiaomi
-TARGET_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
