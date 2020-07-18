63c31c431373d4b70df2e3335cbb2696bb23ffbb
Albert I <kras@raphielgang.org>
2020-07-19 00:50:14 +0800

    vince: init: Update Dalvik heap configuration
    * Use 4 GB configuration for 3/4 GB devices
    * Use 2 GB configuration for 2 GB device
    * Values taken from AOSP 10
    
    Signed-off-by: Albert I <kras@raphielgang.org>
    Signed-off-by: rayrzy <vincentrayananda@gmail.com>

--- init/init_vince.cpp
+++ init/init_vince.cpp
@@ -48,12 +48,9 @@ std::vector<std::string> ro_props_default_source_order = {
         "", "odm.", "product.", "system.", "vendor.",
 };
 
-char const* heapstartsize;
-char const* heapgrowthlimit;
-char const* heapsize;
-char const* heapminfree;
-char const* heapmaxfree;
-char const* heaptargetutilization;
+char const *heaptargetutilization;
+char const *heapminfree;
+char const *heapmaxfree;
 
 void property_override(char const prop[], char const value[], bool add = true) {
     auto pi = (prop_info*)__system_property_find(prop);
@@ -79,35 +76,21 @@ void set_dalvik_props() {
     struct sysinfo sys;
 
     sysinfo(&sys);
-    if (sys.totalram > 5072ull * 1024 * 1024) {
-        // from - phone-xhdpi-6144-dalvik-heap.mk
-        heapstartsize = "16m";
-        heapgrowthlimit = "256m";
-        heapsize = "512m";
-        heaptargetutilization = "0.5";
-        heapminfree = "8m";
-        heapmaxfree = "32m";
-    } else if (sys.totalram > 3072ull * 1024 * 1024) {
-        // from - phone-xxhdpi-4096-dalvik-heap.mk
-        heapstartsize = "8m";
-        heapgrowthlimit = "256m";
-        heapsize = "512m";
+    if (sys.totalram > 2048ull * 1024 * 1024) {
+        // from phone-xhdpi-4096-dalvik-heap.mk
         heaptargetutilization = "0.6";
         heapminfree = "8m";
         heapmaxfree = "16m";
     } else {
-        // from - phone-xhdpi-2048-dalvik-heap.mk
-        heapstartsize = "8m";
-        heapgrowthlimit = "192m";
-        heapsize = "512m";
+        // from phone-xhdpi-2048-dalvik-heap.mk
         heaptargetutilization = "0.75";
         heapminfree = "512k";
         heapmaxfree = "8m";
-    }
+   }
 
-    property_override("dalvik.vm.heapstartsize", heapstartsize);
-    property_override("dalvik.vm.heapgrowthlimit", heapgrowthlimit);
-    property_override("dalvik.vm.heapsize", heapsize);
+    property_override("dalvik.vm.heapstartsize", "8m");
+    property_override("dalvik.vm.heapgrowthlimit", "192m");
+    property_override("dalvik.vm.heapsize", "512m");
     property_override("dalvik.vm.heaptargetutilization", heaptargetutilization);
     property_override("dalvik.vm.heapminfree", heapminfree);
     property_override("dalvik.vm.heapmaxfree", heapmaxfree);
