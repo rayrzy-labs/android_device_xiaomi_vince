ebc8502eb54d2ba14f483210c35f9ba074866186
Sai Kumar Sanagavarapu <ssanagav@codeaurora.org>
2020-10-22 14:09:59 +0200

    vince: rename properties with vendor prefix
    Signed-off-by: rayrzy <vrayananda@yahoo.com>

--- prop.mk
+++ prop.mk
@@ -54,7 +54,6 @@ sys.vendor.shutdown.waittime=500
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
-camera.lowpower.record.enable=1 \
 media.camera.ts.monotonic=1 \
 persist.camera.gyro.disable=0 \
 persist.camera.isp.clock.optmz=0 \
@@ -62,6 +61,7 @@ persist.camera.stats.test=5 \
 persist.vendor.camera.display.lmax=1280x720 \
 persist.vendor.camera.display.umax=1920x1080 \
 persist.vendor.qti.telephony.vt_cam_interface=1 \
+vendor.camera.lowpower.record.enable=1 \
 vidc.enc.dcvs.extra-buff-count=2 \
 persist.camera.HAL3.enabled=1 \
 persist.camera.eis.enable=1 \
