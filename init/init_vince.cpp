93a08a0e8de6dbfb4ddd5b5b4490f67f9626da03
rayrzy <vrayananda@yahoo.com>
2025-07-01 10:24:25 +0700

    vince: clang-code format
    Signed-off-by: rayrzy <vrayananda@yahoo.com>

--- init/init_vince.cpp
+++ init/init_vince.cpp
@@ -26,14 +26,14 @@
    IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#include <android-base/properties.h>
 #include <string.h>
 #include <sys/sysinfo.h>
 #include <unistd.h>
+
 #include <cstdlib>
 #include <fstream>
 #include <vector>
-
-#include <android-base/properties.h>
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
 
@@ -45,80 +45,84 @@ using android::init::property_set;
 using std::string;
 
 std::vector<std::string> ro_props_default_source_order = {
-        "", "odm.", "product.", "system.", "vendor.",
+    "",
+    "odm.",
+    "product.",
+    "system.",
+    "vendor.",
 };
 
-char const *heaptargetutilization;
-char const *heapminfree;
-char const *heapmaxfree;
+char const* heaptargetutilization;
+char const* heapminfree;
+char const* heapmaxfree;
 
 void property_override(char const prop[], char const value[], bool add = true) {
-    auto pi = (prop_info*)__system_property_find(prop);
+  auto pi = (prop_info*)__system_property_find(prop);
 
-    if (pi != nullptr) {
-        __system_property_update(pi, value, strlen(value));
-    } else if (add) {
-        __system_property_add(prop, strlen(prop), value, strlen(value));
-    }
+  if (pi != nullptr) {
+    __system_property_update(pi, value, strlen(value));
+  } else if (add) {
+    __system_property_add(prop, strlen(prop), value, strlen(value));
+  }
 }
 
 void set_avoid_gfxaccel_config() {
-    struct sysinfo sys;
-    sysinfo(&sys);
+  struct sysinfo sys;
+  sysinfo(&sys);
 
-    if (sys.totalram <= 3072ull * 1024 * 1024) {
-        // Reduce memory footprint
-        property_set("ro.config.avoid_gfx_accel", "true");
-    }
+  if (sys.totalram <= 3072ull * 1024 * 1024) {
+    // Reduce memory footprint
+    property_set("ro.config.avoid_gfx_accel", "true");
+  }
 }
 
 void set_dalvik_props() {
-    struct sysinfo sys;
+  struct sysinfo sys;
 
-    sysinfo(&sys);
-    if (sys.totalram > 2048ull * 1024 * 1024) {
-        // from phone-xhdpi-4096-dalvik-heap.mk
-        heaptargetutilization = "0.6";
-        heapminfree = "8m";
-        heapmaxfree = "16m";
-    } else {
-        // from phone-xhdpi-2048-dalvik-heap.mk
-        heaptargetutilization = "0.75";
-        heapminfree = "512k";
-        heapmaxfree = "8m";
-   }
+  sysinfo(&sys);
+  if (sys.totalram > 2048ull * 1024 * 1024) {
+    // from phone-xhdpi-4096-dalvik-heap.mk
+    heaptargetutilization = "0.6";
+    heapminfree = "8m";
+    heapmaxfree = "16m";
+  } else {
+    // from phone-xhdpi-2048-dalvik-heap.mk
+    heaptargetutilization = "0.75";
+    heapminfree = "512k";
+    heapmaxfree = "8m";
+  }
 
-    property_override("dalvik.vm.heapstartsize", "8m");
-    property_override("dalvik.vm.heapgrowthlimit", "192m");
-    property_override("dalvik.vm.heapsize", "512m");
-    property_override("dalvik.vm.heaptargetutilization", heaptargetutilization);
-    property_override("dalvik.vm.heapminfree", heapminfree);
-    property_override("dalvik.vm.heapmaxfree", heapmaxfree);
+  property_override("dalvik.vm.heapstartsize", "8m");
+  property_override("dalvik.vm.heapgrowthlimit", "192m");
+  property_override("dalvik.vm.heapsize", "512m");
+  property_override("dalvik.vm.heaptargetutilization", heaptargetutilization);
+  property_override("dalvik.vm.heapminfree", heapminfree);
+  property_override("dalvik.vm.heapmaxfree", heapmaxfree);
 }
 
 void set_model_props() {
-    const auto set_ro_product_prop = [](const std::string& source, const std::string& prop,
-                                        const std::string& value) {
-        auto prop_name = "ro.product." + source + prop;
-        property_override(prop_name.c_str(), value.c_str(), false);
-    };
+  const auto set_ro_product_prop = [](const std::string& source, const std::string& prop,
+                                      const std::string& value) {
+    auto prop_name = "ro.product." + source + prop;
+    property_override(prop_name.c_str(), value.c_str(), false);
+  };
 
-    string region = GetProperty("ro.boot.hwc", "");
-    string model;
-    if (region == "India") {
-        model = "Redmi Note 5";
+  string region = GetProperty("ro.boot.hwc", "");
+  string model;
+  if (region == "India") {
+    model = "Redmi Note 5";
 
-    } else {
-        model = "Redmi 5 Plus";
-    }
+  } else {
+    model = "Redmi 5 Plus";
+  }
 
-    for (const auto& source : ro_props_default_source_order) {
-        set_ro_product_prop(source, "model", model);
-    }
+  for (const auto& source : ro_props_default_source_order) {
+    set_ro_product_prop(source, "model", model);
+  }
 }
 
 void vendor_load_properties() {
-    set_avoid_gfxaccel_config();
-    set_dalvik_props();
-    set_model_props();
+  set_avoid_gfxaccel_config();
+  set_dalvik_props();
+  set_model_props();
 }
